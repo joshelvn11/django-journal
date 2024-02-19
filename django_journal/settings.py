2d9198197e3adcf564b641b4d368ab1f2b026c33
Josh Beaver <josh.elvn11@gmail.com>
2024-02-19 17:42:05 +0000

    added secret key environ config

--- django_journal/settings.py
+++ django_journal/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bxd)w1=@ibq5g@ws5w8heb44y!xx0++34$m-&$m0p-wn4^e448'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
